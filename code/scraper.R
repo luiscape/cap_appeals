e7c69101793cc4f265092c27b369778b66bc90bc
Luis Capelo <luiscape@gmail.com>
2014-07-04 12:57:15 -0400

    assembling the first list

--- code/scraper.R
+++ code/scraper.R
@@ -11,11 +11,11 @@ library(RCurl)
 
 
 downloadAppeals <- function(verbose = FALSE) {
+    base_url <- 'http://www.unocha.org/cap/appeals/by-appeal/results'
+    page <- '?page='  # 492 appeals in 11 pages -- starts at 0.
+    
     message('Assembling a list of CAP documents.')
-    message('Downloading CAP documents.')
     pb <- txtProgressBar(min = 0, max = 6, style = 3)
-	base_url <- 'http://www.unocha.org/cap/appeals/by-appeal/results'
-	page <- '?page='  # 492 appeals in 11 pages -- starts at 0.
     # CAP appeals go until page 6
     for(i in 0:6) {
         if (verbose == TRUE) message(paste('Page:', i))
@@ -32,24 +32,60 @@ downloadAppeals <- function(verbose = FALSE) {
     cap_list$appeal_type <- 'Consolidated Appeal'
     
     # Flash appeals from page 6 until page 8
-    message('Downloading Flash appeals documents.')
+    message('Assembling a list of Flash appeals documents.')
+    if (verbose == TRUE) message(paste('Page:', i))
     pb <- txtProgressBar(min = 6, max = 8, style = 3)
     for(i in 6:8){
         setTxtProgressBar(pb, i)
+        url <- paste0(base_url, page, i)
+        if (i == 6) { 
+            table <- getNodeSet(htmlParse(url),"//table")[[2]]
+            doc <- readHTMLTable(table, useInternal = TRUE)
+        }
+        else { 
+            doc <- readHTMLTable(getURL(url), useInternal = TRUE)
+        }
+        doc <- data.frame(doc[[1]])
+        doc <- doc[1]
+        colnames(doc)[1] <- 'Document Name'
+        if (i == 6) flash_list <- doc
+        else flash_list <- rbind(flash_list, doc)
     }
+    names(flash_list) <- 'Document Name'
+    flash_list$appeal_type <- 'Flash Appeal'
     
     # Other appeals from page 8 until page 9
-    message('Downloading other appeals documents.')
-    pb <- txtProgressBar(min = 6, max = 8, style = 3)
-    for(i in 6:8){
+    message('Assembling a list of other appeals documents.')
+    pb <- txtProgressBar(min = 8, max = 9, style = 3)
+    for(i in 8:9){
+        if (verbose == TRUE) message(paste('Page:', i))
         setTxtProgressBar(pb, i)
+        url <- paste0(base_url, page, i)
+        if (i == 8) { 
+            table <- getNodeSet(htmlParse(url),"//table")[[2]]
+            doc <- readHTMLTable(table, useInternal = TRUE)
+            doc <- doc[1]
+        }
+        else { 
+            doc <- readHTMLTable(getURL(url), useInternal = TRUE)
+            doc <- data.frame(doc[[1]])
+            doc <- doc[1]
+        }
+        colnames(doc)[1] <- 'Document Name'
+        if (i == 8) other_list <- doc
+        else other_list <- rbind(other_list, doc)
     }
+    names(other_list) <- 'Document Name'
+    other_list$appeal_type <- 'Other appeals'
+    x <<- other_list
     
     message('Done.')
-    return(cap_list)
+    z_list <- rbind(cap_list, flash_list, other_list)
+    return(z_list)
 }
 
 list <- downloadAppeals()
+
 
 # writing CSV. 
 write.csv(list, 'data/appeals_list.csv', row.names = F)
