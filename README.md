29b9ae3f2601db3422671bb92f96ef472cfbd39c
Luis Capelo <luiscape@gmail.com>
2014-07-25 18:51:16 -0400

    adding the distribution per country

--- README.md
+++ README.md
@@ -7,21 +7,27 @@ This repository contains all the code used in the crowd-sourcing initiative to e
 Problem
 -------
 
-There are appeal documents about **74 countries** or regions.
+There are **492** appeals in about **74 countries** or regions. There are *827* documents 
 
 ![Comparing appeals by type.](plot/bar_plot_source.png)
 
 After adding colors to each country, we can see the variety of countries represented in each report group.
 ![Comparing appeals by type and colored by country.](plot/bar_plot_country.png)
 
-Finally, here we can see the distribution of reports per country (or crisis).
+Here we can see the distribution of reports per country (or crisis).
 ![Comparing the type of appeals by country / crisis.](plot/steps_country.png)
+
+South Africa (30) seems to be the country that has the largest number of appeals, followed by South Sudan (26) and Somalia (22).
+![Number of appeals per country.](plot/bar_plot_appeals_per_country.png)
+
 
 
 Solution
 --------
 
-[To come ...]
+
+
+[Crowdcrafting](http://crowdcrafting.org/) is an open-source application created by the [Open Knowledge Foundation](http://blog.okfn.org/2013/09/17/crowdcrafting-putting-citizens-in-control-of-citizen-science/) to make easier the process of creating crowd-sourcing applications.
 
 
 
--- code/plot.R
+++ code/plot.R
@@ -60,3 +60,28 @@ country_plot <- ggplot(data, aes(date, color = appeal_type)) + theme_bw() +
 
 ggsave('plot/steps_country.png', country_plot, height = 9.12, width = 15.93, units = 'in')
 
+
+
+# total appeals per country
+country_s <- as.data.frame(table(data$iso3))
+names(country_s) <- c('iso3', 'count')
+country_s <- country_s[order(- country_s$count, country_s$iso3),]
+
+bar_plot_country <- ggplot(country_s, aes(reorder(country_s$iso3, - country_s$count), count) + 
+    theme_bw() +
+    geom_bar(stat = 'identity', size = 1.3, fill = '#ED8600') +
+#     facet_wrap(~ appeal_type) +
+    ylab("Number of Documents") + xlab("") +
+    theme(panel.border = element_rect(linetype = 0),
+          strip.background = element_rect(colour = "white", fill = "white"),
+          panel.background = element_rect(colour = "white"),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          legend.title = element_blank(),
+          legend.key = element_blank(),
+          legend.key = element_blank(),
+          legend.position = "none",
+          axis.text.x = element_text(angle = 90, hjust = 1),
+          axis.title.y = element_text(size = 7, face = 'italic'))
+
+ggsave('plot/bar_plot_country.png', bar_plot_country, height = 3.05, width = 13.35, units = 'in')
