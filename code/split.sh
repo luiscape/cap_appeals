552a48c79065e4c1579b4a534398945a8da9215e
Luis Capelo <luiscape@gmail.com>
2014-09-19 16:55:23 -0400

    commenting the shell script

--- code/split.sh
+++ code/split.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+
+# this script should be run from the
+# code folder.
+# cd code
+# bash split.sh
+
+# this script reads the PDF documents
+# in the appeal_documents folder
+# and splits them into single PDF documents
+# for every page.
+# it uses the name of the document as a
+# reference when naming the new files.
+
 cd ../data/appeal_documents
 mkdir single_pages
 for f in *.pdf; do
